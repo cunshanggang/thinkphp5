48ff788188c2f604cb3b89465c8946729ad04e0f
cunshanggang <cunshanggang@163.com>
2017-06-28 22:59:26 +0800

    How to upload files.

--- application/study/controller/Index.php
+++ application/study/controller/Index.php
@@ -52,4 +52,28 @@ class Index extends Controller
 
 
     }
+
+    //上传图片(单文件)
+    public function upload(){
+        // 获取表单上传文件 例如上传了001.jpg
+        $file = request()->file('image');
+        // 移动到框架应用根目录/public/uploads/ 目录下
+//        $info = $file->move(ROOT_PATH . 'public' . DS . 'uploads');//没有验证的
+        //加上验证
+        $info = $file->validate(['size'=>15678,'ext'=>'jpg,png,gif'])->move(ROOT_PATH . 'public' . DS . 'uploads');
+        if($info){
+            // 成功上传后 获取上传信息
+            // 输出 jpg
+            echo $info->getExtension();
+            echo "<hr>";
+            // 输出 20160820/42a79759f284b767dfcb2a0197904287.jpg
+            echo $info->getSaveName();//不知道怎么来的
+            echo "<hr>";
+            // 输出 42a79759f284b767dfcb2a0197904287.jpg
+            echo $info->getFilename();
+        }else{
+            // 上传失败获取错误信息
+            echo $file->getError();
+        }
+    }
 }
