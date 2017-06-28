28d891a2a3b3b649936f51f588b397a2da4874f7
cunshanggang <cunshanggang@163.com>
2017-06-28 23:58:59 +0800

    How to import excel.

--- application/study/controller/Index.php
+++ application/study/controller/Index.php
@@ -2,6 +2,7 @@
 namespace app\study\controller;
 use think\Controller;
 use think\Loader;
+use Think\UploadFile;
 use think\Validate;
 class Index extends Controller
 {
@@ -74,6 +75,66 @@ class Index extends Controller
         }else{
             // 上传失败获取错误信息
             echo $file->getError();
+        }
+    }
+
+    //导入数据
+    function impUser(){
+        if (!empty($_FILES)) {
+            import("@.ORG.UploadFile");
+            $config=array(
+                'allowExts'=>array('xlsx','xls'),
+                'savePath'=>'./Public/upload/',
+                'saveRule'=>'time',
+            );
+            $upload = new UploadFile($config);
+            if (!$upload->upload()) {
+                $this->error($upload->getErrorMsg());
+            } else {
+                $info = $upload->getUploadFileInfo();
+
+            }
+
+            vendor("PHPExcel.PHPExcel");
+            $file_name=$info[0]['savepath'].$info[0]['savename'];
+            $objReader = PHPExcel_IOFactory::createReader('Excel5');
+            $objPHPExcel = $objReader->load($file_name,$encode='utf-8');
+            $sheet = $objPHPExcel->getSheet(0);
+            $highestRow = $sheet->getHighestRow(); // 取得总行数
+            $highestColumn = $sheet->getHighestColumn(); // 取得总列数
+            for($i=3;$i<=$highestRow;$i++)
+            {
+                $data['account']= $data['truename'] = $objPHPExcel->getActiveSheet()->getCell("B".$i)->getValue();
+                $sex = $objPHPExcel->getActiveSheet()->getCell("C".$i)->getValue();
+                // $data['res_id']    = $objPHPExcel->getActiveSheet()->getCell("D".$i)->getValue();
+                $data['class'] = $objPHPExcel->getActiveSheet()->getCell("E".$i)->getValue();
+                $data['year'] = $objPHPExcel->getActiveSheet()->getCell("F".$i)->getValue();
+                $data['city']= $objPHPExcel->getActiveSheet()->getCell("G".$i)->getValue();
+                $data['company']= $objPHPExcel->getActiveSheet()->getCell("H".$i)->getValue();
+                $data['zhicheng']= $objPHPExcel->getActiveSheet()->getCell("I".$i)->getValue();
+                $data['zhiwu']= $objPHPExcel->getActiveSheet()->getCell("J".$i)->getValue();
+                $data['jibie']= $objPHPExcel->getActiveSheet()->getCell("K".$i)->getValue();
+                $data['honor']= $objPHPExcel->getActiveSheet()->getCell("L".$i)->getValue();
+                $data['tel']= $objPHPExcel->getActiveSheet()->getCell("M".$i)->getValue();
+                $data['qq']= $objPHPExcel->getActiveSheet()->getCell("N".$i)->getValue();
+                $data['email']= $objPHPExcel->getActiveSheet()->getCell("O".$i)->getValue();
+                $data['remark']= $objPHPExcel->getActiveSheet()->getCell("P".$i)->getValue();
+                $data['sex']=$sex=='男'?1:0;
+                $data['res_id'] =1;
+
+                $data['last_login_time']=0;
+                $data['create_time']=$data['last_login_ip']=$_SERVER['REMOTE_ADDR'];
+                $data['login_count']=0;
+                $data['join']=0;
+                $data['avatar']='';
+                $data['password']=md5('123456');
+                M('Member')->add($data);
+
+            }
+            $this->success('导入成功！');
+        }else
+        {
+            $this->error("请选择上传的文件");
         }
     }
 }
