d370c0cb5ae4b0fa6e6aaaefeba462af11a4bacd
linjiabei <linjb@mama.cn>
2018-08-22 21:11:37 +0800

    TP5 Log.

---
+++ application/index/controller/Logs.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: james
+ * Date: 2018/8/22
+ * Time: 18:24
+ */
+namespace app\index\controller;
+use think\Controller;
+use think\Log;
+class Logs extends Controller
+{
+    public function write()
+    {
+        echo LOG_PATH;
+//        trace('Test Log','error');
+        $log = new Log();
+        $log::write('test log','error');
+//        $this->recordErrorLog();
+//        Log::record('测试日志信息');
+
+        Log::init(['type' => 'File', 'path' => LOG_PATH]);
+//        Log::write('wx支付日志');
+    }
+
+    //tp的日志默认是关闭的，所以下面是初始化日志配置，在日志中记录错误信息
+    private function recordErrorLog()
+    {
+        log::init([
+            'type' => 'File',
+//            'path' => LOG_PATH,
+            'level' => ['error']
+        ]);
+        Log::record('111', 'error');
+    }
+
+
+}
